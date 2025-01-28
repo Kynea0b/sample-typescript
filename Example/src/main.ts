e5731123816d19fb2ab7820c4353c9bef3d9d1d8
Kynea0b <frogandfrog009@gmail.com>
2025-01-28 22:49:46 +0900

    tmp

--- Example/src/main.ts
+++ Example/src/main.ts
@@ -1,5 +1,25 @@
+
+class Person {
+    readonly name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+
+
+
 const main = () => {
     console.log('It works!');
+    let taro: Person 
+    taro = new Person("taro"); // OK
+    console.log(taro);
+
+    let saburo: Person;
+    saburo = { name: "saburo" } // OK
+    console.log(saburo);
+
 };
 
 main();
