266639deab77e1b227a658182aa5bb2f361048f2
kokeshiM0chi <frogandfrog009@gmail.com>
2025-02-15 22:43:20 +0900

    tmp

--- hello-react/my-app/server.py
+++ hello-react/my-app/server.py
@@ -1,9 +1,25 @@
-from flask import Flask, jsonify 
+from flask import Flask, request, jsonify
 from flask_cors import CORS 
 
 app = Flask(__name__) 
 CORS(app) # CORS 設定（実際には http-proxy-middleware で回避） 
 # CORS(app, origins=["http://127.0.0.1:3000", "null"])
+
+
+@app.route('/api/greet', methods=['GET', 'POST'])
+def greet():
+    if request.method == 'POST':
+        name = request.get_json().get('name')  # POSTリクエストからnameを取得
+        message = f"Hello, {name}!"
+        return jsonify({"message": message})
+    else:  # GETリクエストの場合
+        name = request.args.get('name')  # GETリクエストからnameを取得
+        if name:
+            message = f"Hello, {name}!"
+            return jsonify({"message": message})
+        else:
+            return jsonify({"message": "Hello, world!"})  # nameパラメータがない場合のデフォルトメッセージ
+
 
 @app.route("/api/message", methods=["GET"]) 
 def get_message(): 
--- hello-react/my-app/src/App.tsx
+++ hello-react/my-app/src/App.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react"; 
 import { fetchMessage } from "./api"; 
+
+// **** 追加機能
+import axios from "axios";
+interface Message {
+  message: string;
+}
+// ****
+
 const App: React.FC = () => { 
   const [message, setMessage] = useState(""); 
+  // const [message, setMessage] = useState<string>('');
   const handleClick = async () => { 
     try { 
       const data = await fetchMessage(); 
@@ -10,12 +19,48 @@ const App: React.FC = () => {
       console.error("API エラー:", error); 
       setMessage("エラーが発生しました"); 
     } 
-  }; 
+  };
+
+  // **** get/post
+
+  const [greeting, setGreeting] = useState<string>(''); // message を greeting に変更
+  const [name, setName] = useState<string>('');
+  
+  
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleGreetGet = async () => {
+    try {
+      const response = await axios.get<Message>('/api/greet', { params: { name } });
+      setGreeting(response.data.message);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleGreetPost = async () => {
+    try {
+      const response = await axios.post<Message>('/api/greet', { name });
+      setGreeting(response.data.message);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  // ******
   return ( 
   <div> 
     <h1>Flask API からメッセージを取得</h1> 
     <button onClick={handleClick}>メッセージを取得</button> 
-    <p>{message}</p> 
+    <p>{message}</p>
+    <h1>nyannnn</h1> 
+    <input type="text" value={name} onChange={handleNameChange} />
+    <button onClick={handleGreetGet}>Greet (GET)</button>
+    <button onClick={handleGreetPost}>Greet (POST)</button>
+    <p>{greeting}</p>
+
   </div> 
  ); 
 }; 
